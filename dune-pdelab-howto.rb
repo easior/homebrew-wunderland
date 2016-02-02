ae9ba3c90d1a3b6671ebe5918e4b60975e3a4998
Easior Lars <easior.lars@gmail.com>
2016-02-02 10:08:48 +0800

    wxmathplot isn't needed since it's integrated into cb.

--- dune-pdelab-howto.rb
+++ dune-pdelab-howto.rb
@@ -1,14 +1,14 @@
-class Wxmathplot < Formula
-  homepage "http://wxmathplot.sourceforge.net"
-  url "https://github.com/easior/wxMathPlot.git"
+class DunePdelabHowto < Formula
+  desc ""
+  homepage "http://dune-project.org/"
+  url "http://dune-project.org/download/pdelab/2.0/dune-pdelab-howto-2.0.0.tar.xz"
+  version "2.0.0"
+  sha256 "681deb45f649df771b4cd8ec05301645098d6ac9f668e5170021079c1213e1b9"
 
-  version "0.1.3"
-  
   depends_on "cmake"
-  depends_on "wxmac"
-
+  depends_on "easior/wunderland/dune-pdelab"
+  
   def install
-
 
     mkdir "build" do
       args = std_cmake_args
@@ -17,7 +17,7 @@ class Wxmathplot < Formula
       args << "-DCMAKE_C_FLAGS_RELEASE=-O3 -march=native"
       args << "-DCMAKE_CXX_FLAGS_RELEASE=-O3 -march=native"
       args << "-DBUILD_SHARED_LIBS=TRUE"
-      args << "../mathplot"
+      args << ".."
 
       system "cmake", *args
       system "make","install"
