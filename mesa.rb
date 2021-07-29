35f8aa12d2f3a6ee923d855d380ca5593bd4b0ec
Easior Lars <easior.lars@gmail.com>
2021-07-29 09:45:03 +0800

    tc

---
+++ mesa.rb
@@ -0,0 +1,143 @@
+class Mesa < Formula
+  include Language::Python::Virtualenv
+
+  desc "Graphics Library"
+  homepage "https://www.mesa3d.org/"
+  license "MIT"
+  head "https://gitlab.freedesktop.org/mesa/mesa.git", branch: "main"
+
+  stable do
+    url "https://archive.mesa3d.org/mesa-21.1.5.tar.xz"
+    sha256 "022c7293074aeeced2278c872db4fa693147c70f8595b076cf3f1ef81520766d"
+
+    # should be removed in mesa 21.2
+    patch do
+      url "https://gitlab.freedesktop.org/mesa/mesa/-/commit/89b4f337d50c01c5782e19ee40b57a6c9e4b324b.diff"
+      sha256 "e3a7b97e40485d829708bf6013b0755f92f52d00c91bb0f40aa1cdae3cc69e98"
+    end
+  end
+
+  livecheck do
+    url "https://www.mesa3d.org/news/"
+    regex(/>\s*Mesa v?(\d+(?:\.\d+)+) is released\s*</i)
+  end
+
+  bottle do
+    sha256 arm64_big_sur: "c9d78d2751395d2f0643239c52696310a950d574130f5505706f726739d4afa4"
+    sha256 big_sur:       "489c222d695c1bbefa296c26d5bb46c6ce9f3f2a5c18ef87f417d1ac0c8b420e"
+    sha256 catalina:      "6eae667d3f094cc70d2bc6581198385f771e0f82317e2d46ee6592b7bb060b35"
+    sha256 mojave:        "55745e24b051eefda1d4edc13ee20c367c53ecf05ec96047954c2065e8539950"
+    sha256 x86_64_linux:  "a4203fbcfbe04e5c441abe8f4ae9145ff1e5dfd15be2a713039be36c9391800e" # linuxbrew-core
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+  depends_on "expat"
+  depends_on "gettext"
+  depends_on "libx11"
+  depends_on "libxcb"
+  depends_on "libxdamage"
+  depends_on "libxext"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  on_macos do
+    uses_from_macos "llvm"
+  end
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  on_linux do
+  #  depends_on "gcc"
+    depends_on "lm-sensors"
+    depends_on "libelf"
+    depends_on "libxfixes"
+    depends_on "libxrandr"
+    depends_on "libxshmfence"
+    depends_on "libxv"
+    depends_on "libxvmc"
+    depends_on "libxxf86vm"
+    depends_on "libva"
+    depends_on "libvdpau"
+    depends_on "libdrm"
+    depends_on "wayland"
+    depends_on "wayland-protocols"
+  end
+
+  #fails_with gcc: "5"
+
+  resource "Mako" do
+    url "https://files.pythonhosted.org/packages/5c/db/2d2d88b924aa4674a080aae83b59ea19d593250bfe5ed789947c21736785/Mako-1.1.4.tar.gz"
+    sha256 "17831f0b7087c313c0ffae2bcbbd3c1d5ba9eeac9c38f2eb7b50e8c99fe9d5ab"
+  end
+
+  resource "glxgears.c" do
+    url "https://gitlab.freedesktop.org/mesa/demos/-/raw/faaa319d704ac677c3a93caadedeb91a4a74b7a7/src/xdemos/glxgears.c"
+    sha256 "3873db84d708b5d8b3cac39270926ba46d812c2f6362da8e6cd0a1bff6628ae6"
+  end
+
+  resource "gl_wrap.h" do
+    url "https://gitlab.freedesktop.org/mesa/demos/-/raw/faaa319d704ac677c3a93caadedeb91a4a74b7a7/src/util/gl_wrap.h"
+    sha256 "c727b2341d81c2a1b8a0b31e46d24f9702a1ec55c8be3f455ddc8d72120ada72"
+  end
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+
+    venv_root = libexec/"venv"
+    venv = virtualenv_create(venv_root, "python3")
+    venv.pip_install resource("Mako")
+
+    ENV.prepend_path "PATH", "#{venv_root}/bin"
+
+    mkdir "build" do
+      args = ["-Db_ndebug=true"]
+
+      on_linux do
+        args << "-Dplatforms=x11,wayland"
+        args << "-Dglx=auto"
+        args << "-Ddri3=true"
+        args << "-Ddri-drivers=auto"
+        args << "-Dgallium-drivers=auto"
+        args << "-Dgallium-omx=disabled"
+        args << "-Degl=true"
+        args << "-Dgbm=true"
+        args << "-Dopengl=true"
+        args << "-Dgles1=true"
+        args << "-Dgles2=true"
+        args << "-Dxvmc=true"
+        args << "-Dvalgrind=false"
+        args << "-Dtools=drm-shim,etnaviv,freedreno,glsl,nir,nouveau,xvmc,lima"
+      end
+
+      system "meson", *std_meson_args, "..", *args
+      system "ninja"
+      system "ninja", "install"
+    end
+
+    on_linux do
+      # Strip executables/libraries/object files to reduce their size
+      system("strip", "--strip-unneeded", "--preserve-dates", *(Dir[bin/"**/*", lib/"**/*"]).select do |f|
+        f = Pathname.new(f)
+        f.file? && (f.elf? || f.extname == ".a")
+      end)
+    end
+  end
+
+  test do
+    %w[glxgears.c gl_wrap.h].each { |r| resource(r).stage(testpath) }
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -L#{Formula["libx11"].lib}
+      -L#{Formula["libxext"].lib}
+      -lGL
+      -lX11
+      -lXext
+      -lm
+    ]
+    system ENV.cc, "glxgears.c", "-o", "gears", *flags
+  end
+end
